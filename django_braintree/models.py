4af9dc4308d957e136facb505704aef8b1b14233
Nikita Frolenkov <nfrolenkov@gmail.com>
2014-06-24 21:15:26 +0600

    Added initial multi vaults support.

--- django_braintree/models.py
+++ django_braintree/models.py
@@ -11,14 +11,13 @@ class UserVaultManager(models.Manager):
     def get_user_vault_instance_or_none(self, user):
         """Returns a vault_id string or None"""
         qset = self.filter(user=user)
-        if not qset:
+
+        if not qset.exists():
             return None
-        
-        if qset.count() > 1:
-            raise Exception('This app does not currently support multiple vault ids')
-        
-        return qset.get()
-    
+
+        # for compatibility return first result even if we have many
+        return qset[0]
+
     def is_in_vault(self, user):
         return True if self.filter(user=user) else False
     
@@ -30,9 +29,10 @@ class UserVaultManager(models.Manager):
         else:
             user_vault = self.get(user=user)
 
+
 class UserVault(models.Model):
     """Keeping it open that one user can have multiple vault credentials, hence the FK to User and not a OneToOne."""
-    user = models.ForeignKey(User, unique=True)
+    user = models.ForeignKey(User)
     vault_id = models.CharField(max_length=64, unique=True)
     
     objects = UserVaultManager()
